085cc90607b68b7d0ea8a8cb87e042e2ad9af6c6
khris <khris56@hotmail.com>
2019-09-20 16:02:25 -0500

    almost ready type filter

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,11 +2,21 @@ class ExpensesController < ApplicationController
   def index
     puts "Start: #{params[:start]}"
     puts "End: #{params[:end]}"
+    puts "Type: #{params[:type]}"
     @start_date = params[:start].try(:to_date)  || 30.days.ago.to_date
     @end_date = params[:end].try(:to_date) || Date.current
+    @types = params[:type]
     range = (@start_date..@end_date)
-    @expenses = Expense.where(date: range).order(date: :desc)
 
+    if params[:type].nil? || params[:type].empty?
+      @expenses = Expense.where(date: range).order(date: :desc)
+    else
+      date_array = Expense.where(date: range).order(date: :desc)
+      @expenses = date_array.where(type: @types)
+    end
 
+    @num_transactions = Expense.where(date: range).count
+    @total_amount = Expense.where(date: range).sum(:amount)
+    @average = (@total_amount / @num_transactions).round(2)
   end
 end
