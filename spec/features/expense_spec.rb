0d41a5e13661be34a2d8a9e859540838cd277c50
khris <khris56@hotmail.com>
2019-10-17 16:18:23 -0500

    selenium clikcing on new expense, wait 30 and click on submit button working

--- spec/features/expense_spec.rb
+++ spec/features/expense_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 describe "Expense" , type: :feature do
   it "crea un gasto y luego lo envia a expenses path" do
     driver = Selenium::WebDriver.for:chrome
+    driver.manage.timeouts.implicit_wait = 300
     driver.get "http://localhost:3000/expenses"
     driver.find_element(:id, 'new_expense_button').click
+    driver.find_element(:id, 'new_expense_submit').click
+
+    # wait = driver(:timeout => 10)
+    # wait.until { driver.find_element(:id, 'new_expense_button').click }
+
+    # Aqui vamos a tratar de colocar un timeout para dejar que aparezva el modal y llenarlo
   end
 end
