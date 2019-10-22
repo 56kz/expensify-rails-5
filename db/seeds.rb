9bdc00326b1234f1ce876a30fb121bb1dfe0236c
khris <khris56@hotmail.com>
2019-10-22 13:12:10 -0500

    the error on heroku is strf function of sqlite , but the db now is postgress, ready to start th eimplementeation on code to a query

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-#Iteracion entre todos los dias desde hace 6 meses hasta la fecha actual
 (6.months.ago.to_date..Date.current).each do |date|
   rand(2).times do
     Expense.create!(date: date, concept: Faker::Commerce.product_name , amount: Faker::Number.between(5, 900), type: Expense.types.keys.sample, category: Expense.categories.keys.sample)
