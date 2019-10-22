181e4d059c219373fb169b1878e512b225706a0d
khris <khris56@hotmail.com>
2019-10-22 12:29:10 -0500

    bundle exec to deploy on heroku

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id         :integer          not null, primary key
+#  amount     :decimal(, )
+#  category   :integer
+#  concept    :string
+#  date       :date
+#  type       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :expense do
     amount { rand(5..200) }
